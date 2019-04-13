6820042a9875c1e97f90a277584b5872e529917a
Siena Ha <sha021@ucr.edu>
2019-04-13 01:09:02 -0700

    part5

--- Lab4/sha021_lab4_part5/sha021_lab4_part5/main.c
+++ Lab4/sha021_lab4_part5/sha021_lab4_part5/main.c
@@ -2,8 +2,8 @@
  * Seung Eun Ha - sha021@ucr.edu
  * Lab Section: 27
  * Assignment: Lab 4 Part 4
- * Description: Changing code from the previous "Combinational Lock" program to more complex lock code
- * 
+ * Description: Changing code from the previous "Combinational Lock" program to more complex lock code;
+ *              also removing states and changing to Looping State Machine
  *
  * Created: 4/12/2019 11:10:05 PM
  * Author : SIENA
@@ -15,12 +15,10 @@
 enum Lock_State { Start, Init, Locked, Unlocked, Code } Lock_State;
     
 void combinationalLock() {
-    
-    unsigned char x, y, pound, lock, deadBolt;
-    x = PINA & 0x01;
-    y = PINA & 0x02;
-    pound = PINA & 0x04;
-    lock = PINA & 0x80;
+
+    const unsigned char THE_CODE[3] = { /*#,*/ 0x01, 0x02, 0x01 }; 
+    unsigned char numPad, deadBolt, correctCode, i;
+    numPad = PINA;
     deadBolt = PINB;
     
     switch(Lock_State) {
@@ -32,52 +30,37 @@ void combinationalLock() {
             Lock_State = Locked;
         case Locked:
             deadBolt = 0x00;
-            if (pound && !x && !y && !lock) Lock_State = PressP_1;
+            if (numPad == 0x04) Lock_State = Code;
             else Lock_State = Locked;
-            break;
-        case PressP_1:
-            deadBolt = deadBolt? 0x01 : 0x00;
-            state = 1;
-            // Stay in this state
-            if (pound && !x && !y && !lock) Lock_State = PressP_1;
-            // Transition to new state
-            else {
-                // Correct sequence
-                if (!pound && !x && !y && !lock) Lock_State = ReleaseP_2;
-                // Code Wrong while attempting to Lock
-                else if (deadBolt) {
-                    Lock_State = Unlocked;
-                }
-                // Code Wrong while attempting to Unlock
-                else {
-                    Lock_State = Locked;
-                } 
-            }            
-            break;
-        case ReleaseP_2:
-            deadBolt = deadBolt? 0x01 : 0x00;
-            state = 2;
-            // Stay in this state
-            if (!pound && !x && !y && !lock) Lock_State = ReleaseP_2;
-            // Transition to new state
-            else {
-                // Attempting to Lock
-                if (deadBolt) {
-                    if (!pound && !x && y && !lock) Lock_State = Locked;
-                    else Lock_State = Unlocked;
-                }
-                // Attempting to Unlock
-                else {
-                    if (!pound && !x && y && !lock) Lock_State = Unlocked;
-                    else Lock_State = Locked;
-                }                
-            }                
             break;
         case Unlocked:
             deadBolt = 0x01;            
-            if (!pound && !x && !y && lock) Lock_State = Locked;
-            else if (pound && !x && !y && !lock) Lock_State = PressP_1;
+            if (numPad == 0x80) Lock_State = Locked;
+            else if (numPad == 0x04) Lock_State = Code;
             else Lock_State = Unlocked;
+            break;
+        case Code:
+            deadBolt = deadBolt? 0x01 : 0x00;
+            correctCode = 1;
+            // While # key is pressed
+            while (numPad == 0x04) numPad = PINA;
+            // After releasing # key
+            for (i = 0; i < 3; i++) {
+                numPad = PINA;
+                // While previous button is being held
+                if (i > 0) {
+                    while (numPad == THE_CODE[i - 1]) numPad = PINA;
+                }
+                // While no button pressed
+                while (!numPad) numPad = PINA;
+                // Wrong lock code combo
+                if (numPad != THE_CODE[i]) {
+                    correctCode = 0;
+                    break;
+                }
+            }
+            if (correctCode) Lock_State = deadBolt ? Locked : Unlocked;
+            else Lock_State = deadBolt ? Unlocked : Locked;
             break;
         default:
             Lock_State = Init;
@@ -96,4 +79,3 @@ int main(void)
         combinationalLock();
     }
 }
-
