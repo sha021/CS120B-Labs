069436b5889d51b9504d6527c3f92ac1936880a9
Nathan Brennan <nbren004@ucr.edu>
2019-05-05 21:33:11 -0700

    Lab 9 part 2 done

---
+++ Includes_PWM/frequencies.h
@@ -0,0 +1,81 @@
+#define REST 0
+
+#define C2   65.41
+#define C2_S 69.30
+#define D2   73.42
+#define D2_S 77.78
+#define E2   82.41
+#define F2   87.31
+#define F2_S 92.50
+#define G2   98.00
+#define G2_S 103.83
+#define A2   110.00
+#define A2_S 116.54
+#define B2   123.47
+
+#define C3   130.81
+#define C3_S 138.59
+#define D3   146.83
+#define D3_S 155.56
+#define E3   164.81
+#define F3   174.61
+#define F3_S 185.00
+#define G3   196.00
+#define G3_S 207.65
+#define A3   220.00
+#define A3_S 233.08
+#define B3   246.94
+
+#define C4   261.63
+#define C4_S 277.18
+#define D4   293.66
+#define D4_S 311.13
+#define E4   329.63
+#define F4   349.23
+#define F4_S 369.99
+#define G4   392.00
+#define G4_S 415.30
+#define A4   440.00
+#define A4_S 466.16
+#define B4   (493.88)
+
+#define C5   (523.25)
+#define C5_S 554.37
+#define D5   587.33
+#define D5_S 622.25
+#define E5   (659.25)
+#define F5   698.46
+#define F5_S 739.99
+#define G5   783.99
+#define G5_S 830.61
+#define A5   880.00
+#define A5_S 932.33
+#define B5   987.77
+
+#define C6   1046.50
+#define C6_S 1108.73
+#define D6   1174.66
+#define D6_S 1244.51
+#define E6   1318.51
+#define F6   1396.91
+#define F6_S 1479.98
+#define G6   1567.98
+#define G6_S 1661.22
+#define A6   1760.00
+#define A6_S 1864.66
+#define B6   1975.53
+
+#define C7   2093.00
+#define C7_S 2217.46
+#define D7   2349.32
+#define D7_S 2489.02
+#define E7   2637.02
+#define F7   2793.83
+#define F7_S 2959.96
+#define G7   3135.96
+#define G7_S 3322.44
+#define A7   3520.00
+#define A7_S 3729.31
+#define B7   3951.07
+
+
