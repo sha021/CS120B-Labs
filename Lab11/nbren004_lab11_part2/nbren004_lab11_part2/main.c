f249e966488894becffef121826725c874e64417
Nathan Brennan <nbren004@ucr.edu>
2019-05-19 01:51:23 -0700

    Lab 11 Part 1

---
+++ Lab11/nbren004_lab11_part2/nbren004_lab11_part2/main.c
@@ -0,0 +1,23 @@
+ /* Nathan Brennan - nbren004@ucr.edu
+ * Seung Eun Ha - sha021@ucr.edu
+ * Lab Section: 27
+ * Assignment: Lab 11 Part 1
+ * Description: Display "How I Met Your Mother" reference on LCD
+ *
+ * Created: 5/19/2019 1:25:54 AM
+ * Author : Nathan Brennan & Seung Eun Ha
+ */ 
+
+
+
+#include <avr/io.h>
+
+
+int main(void)
+{
+    /* Replace with your application code */
+    while (1) 
+    {
+    }
+}
+
